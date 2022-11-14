46416b585f82778d210ec7b3a642bfd5525b0a5a
CristianoAltomonte <cristianoaltomonte@hotmail.it>
2022-11-14 10:08:36 +0000

    Bonus

--- main.js
+++ main.js
@@ -2,15 +2,19 @@ var app = new Vue({
     el: '#app',
     data: {
         obj: [
-            { image: 'https://images.unsplash.com/photo-1668164077013-0e7dddb0c110?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80'},
-            { image: 'https://images.unsplash.com/photo-1664574653790-cee0e10a4242?ixlib=rb-4.0.3&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80'},
-            { image: 'https://plus.unsplash.com/premium_photo-1661503210390-8fc97a27eaa6?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80'},
-            { image: 'https://images.unsplash.com/photo-1661956601031-4cf09efadfce?ixlib=rb-4.0.3&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1776&q=80'}
+            { image: 'https://images.unsplash.com/photo-1668164077013-0e7dddb0c110?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80' },
+            { image: 'https://images.unsplash.com/photo-1664574653790-cee0e10a4242?ixlib=rb-4.0.3&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80' },
+            { image: 'https://plus.unsplash.com/premium_photo-1661503210390-8fc97a27eaa6?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80' },
+            { image: 'https://images.unsplash.com/photo-1661956601031-4cf09efadfce?ixlib=rb-4.0.3&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1776&q=80' }
         ],
 
         indice: 0,
     },
-    
+
+    mounted() {
+        this.attivaIntervallo();
+    },
+
     methods: {
         next: function () {
 
@@ -33,17 +37,27 @@ var app = new Vue({
 
             let lunghezza = this.obj.length - 1;
 
-            if (this.indice >= lunghezza) {
+            if (this.indice <= 0) {
 
-                return this.indice = 1;
+                return this.indice = obj.length - 1;
 
             }
             else {
 
-                return this.indice --;
+                return this.indice--;
 
             }
 
+        },
+
+        attivaIntervallo() {
+            setInterval(() => { this.next() }, 10000);
+
+        },
+
+        gallery(index) {
+            this.indice = index;
         }
+
     }
 })
